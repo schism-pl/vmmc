13b6ced3951c14a1ff4e0de6a1a23dde734e8e6e
Evan Johnson <e5johnso@eng.ucsd.edu>
2024-03-07 12:55:22 -0800

    add first attempt and angular patch potential

--- src/morphology.rs
+++ src/morphology.rs
@@ -2,6 +2,8 @@ use std::{f64::consts::PI, fmt};
 
 use serde::{de, Deserialize, Serialize};
 
+use crate::consts::PARTICLE_RADIUS;
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Patch {
     radius: f64,  // radius of patch (in units of particle diameter)
@@ -29,6 +31,14 @@ impl Patch {
     pub fn chemtype(&self) -> u8 {
         self.chemtype
     }
+
+    /// TODO: add derivation here
+    /// +- radians where it can still interact
+    ///  = +- acos 1-(patch_radius^2/2)
+    pub fn angle_tolerance(&self) -> f64 {
+        let r = self.radius;
+        1.0 - (r * r) / (2.0 * PARTICLE_RADIUS * PARTICLE_RADIUS)
+    }
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -42,6 +52,10 @@ pub struct Morphology {
     // maps theta -> cos(theta) for each patch angle
     #[serde(skip_serializing)]
     cos_theta: Vec<f64>,
+    // TODO: better docs
+    // maps patch -> angle tolerance (+- radians where it can still interact)
+    #[serde(skip_serializing)]
+    angle_tolerances: Vec<f64>,
 }
 
 impl<'de> de::Deserialize<'de> for Morphology {
@@ -84,10 +98,12 @@ impl Morphology {
 
         let mut sin_theta: Vec<_> = Vec::new();
         let mut cos_theta: Vec<_> = Vec::new();
+        let mut angle_tolerances: Vec<_> = Vec::new();
         for patch in &patches {
             let theta = patch.theta() * PI / 180.0; // convert degrees to radians
             sin_theta.push(theta.sin());
             cos_theta.push(theta.cos());
+            angle_tolerances.push(patch.angle_tolerance());
         }
 
         Self {
@@ -95,6 +111,7 @@ impl Morphology {
             max_radius,
             sin_theta,
             cos_theta,
+            angle_tolerances,
         }
     }
 
@@ -112,6 +129,22 @@ impl Morphology {
 
     pub fn cos_theta(&self, patch_idx: usize) -> f64 {
         self.cos_theta[patch_idx]
+    }
+
+    // TODO: can probably be optimized
+    /// given an angle, the patch that contains the angle
+    /// if there is no such patch, returns None
+    pub fn closest_patch(&self, theta: f64) -> Option<&Patch> {
+        for (p_idx, patch) in self.patches.iter().enumerate() {
+            let patch_theta = patch.theta() * PI / 180.0; // convert degrees to radians
+                                                          // TODO: use in_range?
+            let angle_tolerance = self.angle_tolerances[p_idx];
+            if theta >= patch_theta - angle_tolerance && theta < patch_theta + angle_tolerance {
+                // TODO: wrap-around?
+                return Some(patch);
+            }
+        }
+        None
     }
 
     pub fn regular_3patch(radius: f64) -> Self {
--- src/patchy_discs.rs
+++ src/patchy_discs.rs
@@ -34,6 +34,7 @@ impl PatchyDiscsPotential {
         let p1 = particle1.pos();
 
         // norm_sqd < 1.0 => norm < 1.0
+        // TODO: explicity calc diff here (d0 in steve's code)
         let dist_sqd = simbox.sep_in_box(p0, p1).l2_norm_sqd();
 
         // overlap!
@@ -41,7 +42,11 @@ impl PatchyDiscsPotential {
             return f64::INFINITY;
         }
 
-        if dist_sqd.sqrt() > PARTICLE_DIAMETER + m0.max_patch_radius() + m1.max_patch_radius() {
+        // TODO: change for particle overlap (to max)
+        // if dist_sqd.sqrt() > PARTICLE_DIAMETER + m0.max_patch_radius() + m1.max_patch_radius() {
+        //     return 0.0;
+        // }
+        if dist_sqd.sqrt() > PARTICLE_DIAMETER + m0.max_patch_radius().max(m1.max_patch_radius()) {
             return 0.0;
         }
 
@@ -58,7 +63,8 @@ impl PatchyDiscsPotential {
                 let pc1 = simbox.patch_center(particle1, p_idx1);
                 let patch_dist = simbox.sep_in_box(pc0, pc1).l2_norm();
 
-                if patch_dist < patch0.radius() + patch1.radius() {
+                // TODO: change to min for patch overlap
+                if patch_dist < patch0.radius().max(patch1.radius()) {
                     // theres no way for more than 2 patches to interact between 2 particles
                     // TODO: write out exact conditions for this and assert it
                     // appendix E of https://journals.aps.org/prx/pdf/10.1103/PhysRevX.4.011044
@@ -67,6 +73,88 @@ impl PatchyDiscsPotential {
             }
         }
 
+        0.0
+    }
+
+    pub fn determine_interactions(&self, simbox: &SimBox, p: &Particle) -> Vec<ParticleId> {
+        // log::debug!("Determining interactions for p{:?}", p.id());
+        let mut interactions = Vec::new();
+        for neighbor_id in simbox.get_neighbors(p.pos()) {
+            let neighbor = simbox.particle(neighbor_id); //&simbox.particles()[neighbor_id as usize];
+            if neighbor == p {
+                continue;
+            }
+            let energy = self.compute_pair_energy(simbox, p, neighbor);
+
+            // particles interact!
+            if energy < 0.0 {
+                let m = simbox.morphology(p);
+                assert_ne!(interactions.len(), m.patches().len());
+                interactions.push(neighbor_id);
+            }
+        }
+
+        interactions
+    }
+}
+
+pub struct AngularPatchyDiscsPotential {
+    interaction_energy: f64,
+}
+
+impl AngularPatchyDiscsPotential {
+    pub fn new(interaction_energy: f64) -> Self {
+        Self { interaction_energy }
+    }
+
+    pub fn interaction_energy(&self) -> f64 {
+        self.interaction_energy
+    }
+
+    pub fn set_interaction_energy(&mut self, interaction_energy: f64) {
+        self.interaction_energy = interaction_energy
+    }
+
+    pub fn compute_pair_energy<P1: IsParticle, P2: IsParticle>(
+        &self,
+        simbox: &SimBox,
+        particle0: &P1,
+        particle1: &P2,
+    ) -> f64 {
+        let m0 = simbox.morphology(particle0);
+        let m1 = simbox.morphology(particle1);
+
+        let p0 = particle0.pos();
+        let p1 = particle1.pos();
+
+        let diff = simbox.sep_in_box(p0, p1);
+
+        // norm_sqd < 1.0 => norm < 1.0
+        let dist = diff.l2_norm_sqd().sqrt();
+
+        // overlap!
+        if dist < PARTICLE_DIAMETER {
+            return f64::INFINITY;
+        }
+
+        if dist > PARTICLE_DIAMETER + m0.max_patch_radius().max(m1.max_patch_radius()) {
+            return 0.0;
+        }
+
+        let or0 = particle0.or();
+        let or1 = particle1.or();
+
+        // calculate angle between difference vector and particle orientationt to find closest patch
+        let angle0 = diff.dot_prod(or0).acos();
+        let angle1 = diff.dot_prod(or1).acos();
+
+        if let Some(patch0) = m0.closest_patch(angle0) {
+            if let Some(patch1) = m1.closest_patch(angle1) {
+                if dist <= PARTICLE_DIAMETER + patch0.radius().max(patch1.radius()) {
+                    return -self.interaction_energy;
+                }
+            }
+        }
         0.0
     }
 
